69a66a5889bf93259e442dbec4390263bbf2117a
Jared Sackett <jake850@gmail.com>
2018-12-04 03:16:53 +0000

    Functional parser to turn a textbox into first letters with underline/not feature.

---
+++ resources/js/components/App-MemorizeTool.vue.js
@@ -0,0 +1,65 @@
+export default {
+  name: 'MemorizeTool',
+  data: () => {
+    return {
+      inputText: '',
+      underlineMode: true
+    }
+  },
+
+  computed: {
+    outputText: function () {
+      if (this.inputText) {
+        var lines = this.inputText.split('\n')
+
+        var linesArray = lines.map((line) => {
+            return this.joinFancy(line.replace(/[^\w\s]|_/g, function ($1) { return ' ' + $1 + ' ' }).replace(/[ ]+/g, ' ').split(' ').map((word) => {
+              if (word.length > 1 && this.underlineMode) {
+                let wordStart = word.slice(0, 1)
+                let wordEnd = this.repeatChar('_', word.length - 1)
+                return wordStart + wordEnd
+              } else {
+                return word.slice(0, 1)
+              }
+            }))
+        })
+        return linesArray.join('<br>')
+      } else {
+        return 'Type something!'
+      }
+    }
+  },
+
+  methods: {
+    repeatChar (pattern, count) {
+      if (count < 1) return ''
+      var result = ''
+      while (count > 1) {
+        if (count & 1) result += pattern
+        // eslint-disable-next-line
+        count >>= 1, pattern += pattern
+      }
+      return result + pattern
+    },
+
+    joinFancy (arrayThing) {
+      var outputString = ''
+      arrayThing.forEach((thing, i) => {
+        console.log(thing)
+        // put a space after UNLESS the next item is a punctuation mark
+        if (i === (arrayThing.length - 1)) {
+          // It's the last item, so we can't check the next thing
+          outputString += thing
+        } else {
+          var nextThing = arrayThing[i+1].slice(0,1)
+          if (nextThing.match(/[?!.,]/)) {
+            outputString += thing
+          } else {
+            outputString += thing + ' '
+          }
+        }
+      })
+      return outputString
+    }
+  }
+}
