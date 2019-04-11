15bc8fd2df7e197dc423c9f0544f7f7604d73614
Jared Sackett <jake850@gmail.com>
2019-04-11 18:16:17 +0000

    Fixed typo on homepage blurb

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     Looking to memorize some verbatim text? One of the best methods for memorizing anything is to practice recalling it, instead of just repeating it. This app helps you do that by showing you just the first letter of each word. That narrows the options and gives you some structure for your brain to try and remember, without just giving you the answers. Simply copy and paste some text in the box below!
   </p>
   <p>
-    Check back if you're interested future features, which will include things like:
+    Check back if you're interested in future features, which will include things like:
     <ul>
       <li>Saving your text blurbs</li>
       <li>A spaced-repetition-system style utility to ensure you ALWAYS remember what you're trying to remember</li>
